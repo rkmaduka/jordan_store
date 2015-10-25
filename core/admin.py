7fbd7d24bfd1d4b24a5a1028d69197e71630c5e4
rkmaduka <rkmaduka@quinnipiac.edu>
2015-10-25 15:51:02 +0000

    Add requests to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Request)
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -4,5 +4,6 @@ class Request(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    def __unicode__(self):
+      return self.title
 # Create your models here.
